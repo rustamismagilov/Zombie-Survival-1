c0a0396c8ceaeaf9281d3a2d6d2c58c18eda25f1
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2024-06-26 01:30:24 +0200

    Bug fixes + weapon differentiation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -15,9 +15,20 @@ public class Weapon : MonoBehaviour
     [SerializeField] AmmoType ammoType;
     [SerializeField] TextMeshProUGUI ammoText;
     [SerializeField] Animator weaponAnimator;
+    [SerializeField] float bulletSpread = 0.1f;
+    [SerializeField] int numberOfPellets = 10;
 
 
     public bool canShoot = true;
+
+    public enum WeaponType
+    {
+        RegularGun,
+        AssaultRifle,
+        Shotgun
+    }
+
+    [SerializeField] WeaponType weaponType;
 
     private void OnEnable()
     {
@@ -43,7 +54,26 @@ public class Weapon : MonoBehaviour
         {
             PlayMuzzleFlash();
             GetComponent<AudioSource>().Play();
-            ProcessRaycast();
+
+            switch (weaponType)
+            {
+                case WeaponType.RegularGun:
+                    ProcessRaycast();
+                    // Here you can also handle additional logic for this weapon if needed
+                    break;
+                case WeaponType.AssaultRifle:
+                    ProcessRaycast();
+                    // Here you can also handle additional logic for this weapon if needed
+                    break;
+                case WeaponType.Shotgun:
+                    for (int i = 0; i < numberOfPellets; i++)
+                    {
+                        ProcessRaycast();
+                        // Here you can also handle additional logic for this weapon if needed
+                    }
+                    break;
+            }
+
             ammoSlot.ReduceCurrentAmmo(ammoType);
             weaponAnimator.SetTrigger("shoot");
         }
@@ -55,8 +85,9 @@ public class Weapon : MonoBehaviour
 
     void ProcessRaycast()
     {
+        Vector3 direction = GetSpreadDirection();
         RaycastHit hit;
-        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        if (Physics.Raycast(camera.transform.position, direction, out hit, range))
         {
             CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
@@ -85,5 +116,13 @@ public class Weapon : MonoBehaviour
     {
         int currentAmmo = ammoSlot.GetCurrentAmmo(ammoType);
         ammoText.text = currentAmmo.ToString();
+    }
+
+    Vector3 GetSpreadDirection()
+    {
+        Vector3 direction = camera.transform.forward;
+        direction.x += Random.Range(-bulletSpread, bulletSpread);
+        direction.y += Random.Range(-bulletSpread, bulletSpread);
+        return direction;
     }
 }
